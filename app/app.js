20e37ef04343af43839ecec951bb0b02771bf2d0
Chris Barrett <c.barrett@me.com>
2016-06-04 13:55:20 -0500

    first attempt at auth

--- app/app.js
+++ app/app.js
@@ -1,6 +1,15 @@
-"use strict"
-
 var app = angular.module("MovieHistory", ["ngRoute"])
+  .constant("firebaseURL","https://groovymoviehistory.firebaseio.com/");
+
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+  if(AuthFactory.isAuthenticated()){
+    console.log("User is authenticated, resolve route promise");
+    resolve();
+  } else {
+    console.log("User is not authenticated, reject route promise");
+    reject();
+  }
+})
 
 app.config(function($routeProvider){
     $routeProvider.
@@ -22,3 +31,14 @@ app.config(function($routeProvider){
         // }).
         otherwise("/");
 });
+
+app.run(($location) =>{
+  let movieRef = new Firebase("https://groovymoviehistory.firebaseio.com/");
+
+  movieRef.onAuth(authData =>{
+    if(!authData){
+      $location.path("/login");
+    }
+  })
+})
+
