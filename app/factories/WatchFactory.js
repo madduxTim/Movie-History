8c1145ed60eb647af6398a7c14837df26cedd34d
complikatyed <katye.russell@gmail.com>
2016-06-04 21:32:19 -0500

    Add user id to Firebase save

--- app/factories/WatchFactory.js
+++ app/factories/WatchFactory.js
@@ -1,8 +1,10 @@
 'use strict';
 
-app.factory("watchListStorage", function($q, $http, firebaseURL) {
+app.factory("watchListStorage", function($q, $http, firebaseURL, AuthFactory) {
 
   var postNewWatchMovie = function(newWatchMovie) {
+
+    let user = AuthFactory.getUser();
 
     return $q(function(resolve, reject) {
       $http
@@ -11,6 +13,7 @@ app.factory("watchListStorage", function($q, $http, firebaseURL) {
             title: newWatchMovie.title,
             year: newWatchMovie.year,
             rating: newWatchMovie.rating,
+            uid: user.uid
           }))
         .success(function(objectFromFirebase) {
           resolve(objectFromFirebase);
