41fb0c3131148267cbf3dc3719dbd05577926d14
madduxTim <timmaddux@Tims-MacBook-Pro.local>
2016-06-06 15:15:51 -0500

    Search Button connected to API call

--- app/factories/CallFactory.js
+++ app/factories/CallFactory.js
@@ -1,8 +1,8 @@
 "use strict";
-app.factory("movieQueryStore", function($q, $http, OMDBurl){
-    var getSearchMovies = function(){
+app.factory("movieQueryStore", function($q, $http){
+    var movieSearchCall = function(){
         let queryStorage = [];
-        let searchTerms = "Star";        
+        let searchTerms = document.getElementById("omdbSearch").value;      
         return $q(function(resolve, reject){
             $http.get("http://www.omdbapi.com/?s="+searchTerms+"&y=&plot=short&r=json")
                 .success(function(queryData){
@@ -18,5 +18,5 @@ app.factory("movieQueryStore", function($q, $http, OMDBurl){
 
     });
 }
-    return {getSearchMovies:getSearchMovies};
+    return {movieSearchCall:movieSearchCall};
 }); 
