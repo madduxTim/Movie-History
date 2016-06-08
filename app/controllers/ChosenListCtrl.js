f71f50b2a6735af68a8bc906400792b247040419
Chris Barrett <c.barrett@me.com>
2016-06-08 12:35:40 -0500

    improved scrolling

--- app/controllers/ChosenListCtrl.js
+++ app/controllers/ChosenListCtrl.js
@@ -9,20 +9,10 @@ app.controller('ChosenListCtrl', function($scope, $location, chosenStorage){
   });
 
   $scope.rankMovie = function(movieToRank){
-
     chosenStorage.rankChosenMovie(movieToRank)
       .then(function successCallback(response) {
-        $location.url("/list");
+        $location.url("/");
       });
-  };
-
-  $scope.deleteMovie = function(movieId, movieTitle){
-    chosenStorage.deleteChosenMovie(movieId).then(function(response){
-      chosenStorage.getChosenMovieList().then(function(someCollection){
-        $scope.chosenMovies = someCollection;
-        Materialize.toast(`${movieTitle} removed from your list`, 3000, "rounded");
-      });
-    });
   };
 
   $scope.deleteMovie = function(movieId, movieTitle){
